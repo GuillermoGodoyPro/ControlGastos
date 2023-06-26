0284a2db2ab7bf6f32080dc7c781eb924aad5046
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-06-25 21:45:20 -0300

    update Modal animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,15 @@ function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [ isValidPresupuesto, setIsValidPresupuesto ] = useState(true)
   const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
 
   const handleNuevoGasto = () =>{
-      console.log("diste click para nuevo gasto")
       setModal(true)
+
+      setTimeout(() => {
+        setAnimarModal(true)
+      }, 500)
+
   }
 
   return (
@@ -33,7 +38,7 @@ function App() {
     )
     : null }
 
-      {modal && <Modal/>}
+      {modal && <Modal setModal={setModal} animarModal={animarModal} setAnimarModal={setAnimarModal} />}
     </div>
   )
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = () => {
-  return (
-    <div>Modal</div>
+const Modal = ({setModal, animarModal, setAnimarModal}) => {
+
+    const ocultarModal = () => {
+        setAnimarModal(false)
+
+        setTimeout(() => {
+            setModal(false)
+        }, 500)
+    }
+
+    
+    return (
+        <div className="modal">
+            <div className="cerrar-modal">
+                <img
+                    src={CerrarBtn}
+                    alt='cerrar modal'
+                    onClick={ocultarModal}
+                />
+            </div>
+
+            <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
+                <legend>Nuevo Gasto</legend>
+            </form>
+
+        </div>
   )
 }
 
