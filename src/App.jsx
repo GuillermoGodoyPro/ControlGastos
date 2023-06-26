9c6c33f07863d67941f904697f37f1c8484d7c3b
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-06-25 21:21:15 -0300

    add Modal Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
-  const [ isValidPresupuesto, setIsValidPresupuesto ] = useState(false)
+  const [ isValidPresupuesto, setIsValidPresupuesto ] = useState(true)
+  const [modal, setModal] = useState(false)
+
+  const handleNuevoGasto = () =>{
+      console.log("diste click para nuevo gasto")
+      setModal(true)
+  }
 
   return (
     <div>
@@ -20,11 +27,13 @@ function App() {
         <img 
           src={IconoNuevoGasto}
           alt='icono nuevo gasto'
+          onClick={handleNuevoGasto}
         />
       </div>
     )
     : null }
 
+      {modal && <Modal/>}
     </div>
   )
 }
